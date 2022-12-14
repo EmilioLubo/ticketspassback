65042d247d7ec1a58bd09d9979801ab194fbe58c
ivan_gutierrez <ivan.gutierrezc92@gmail.com>
2022-12-14 13:43:42 -0300

    Modificado modelos de concierto

--- models/Concert.js
+++ models/Concert.js
@@ -3,18 +3,20 @@ const mongoose = require("mongoose");
 const categorySchema = new mongoose.Schema({
   name: { type: String, required: true },
   price: { type: Number, required: true },
-  currency: {type: String, required: true},
   area: { type: String },
 });
 
 const schema = new mongoose.Schema({
   name: { type: String, required: true },
   photo: { type: String, required: true },
+  banner: { type: String, required: true },
   artists: [{ type: mongoose.Types.ObjectId, ref: "artists", required: true }],
   date: { type: Date, required: true },
+  youtubeVideo: {type: String},
+  description: {type: String, required: true},
   venue: { type: mongoose.Types.ObjectId, ref: "venues", required: true },
   type: { type: String, required: true },
-  category: [categorySchema],
+  category: {type: categorySchema, required: true}
 });
 
 const Concert  = mongoose.model("concerts", schema);
