c858e8422ab65af9cec6a7f9bc97614bee291fea
Emilio Lubo <emilubo@msn.com>
2022-12-14 14:35:58 -0300

    validation on edit artist route

--- routes/artist.js
+++ routes/artist.js
@@ -1,7 +1,6 @@
 const router = require('express').Router()
 const {create, read, update, destroy} = require('../controllers/artist')
 const schema = require('../schemas/artist')
-const editSchema = require('../schemas/editArtist')
 const validator = require('../middlewares/validator')
 const passport = require('../config/passport')
 const nameAlreadyExist = require('../middlewares/nameAlreadyExist')
@@ -10,7 +9,7 @@ const model = require('../models/Artist')
 router.post('/', passport.authenticate("jwt", { session: false }), validator(schema), nameAlreadyExist(model), create)
 router.get('/', read)
 router.get('/:id', read)
-router.patch('/:id', passport.authenticate("jwt", { session: false }), validator(editSchema), update)
+router.patch('/:id', passport.authenticate("jwt", { session: false }), validator(schema), update)
 router.delete('/:id', passport.authenticate("jwt", { session: false }), destroy)
 
 module.exports = router
